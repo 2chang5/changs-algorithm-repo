87461643d95e1c1b065d561b201cb3cdaf3de3d5
changhwan <chang5hustle@gmail.com>
2022-05-18 15:51:01 +0900

    2022.05.18

--- 2022.05.11/10974.py
+++ 2022.05.11/10974.py
@@ -1,5 +1,4 @@
 import itertools
-import itertools
 
 n = int(input())
 
---
+++ 2022.05.18/10807.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+arr = list(map(int,input().split()))
+
+v = int(input())
+
+ans = 0
+
+for i in arr:
+    if i == v:
+        ans += 1
+
+print(ans)
